374f8c6beea12706be704f5228db66702620fe96
zhanghaowei123 <2904845562@qq.com>
2021-06-18 14:39:06 +0800

    finish

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
